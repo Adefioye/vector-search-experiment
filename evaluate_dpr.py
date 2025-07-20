235576d002a252c7d1fb84dc42f1f583984dac79
abdulhakeem <abdulhakeemadefioye@gmail.com>
2025-07-19 22:28:19 -0500

    Compute number of parameters of embedding models and deleted unused results

--- evaluate_dpr.py
+++ evaluate_dpr.py
@@ -10,15 +10,16 @@ import torch, gc
 torch.cuda.empty_cache()
 gc.collect()
 
-model_name = "kokolamba/ModernBERT-large-DPR-8e-05-CMNRL-minibs64"
+model_name = "answerdotai/ModernBERT-base"
 lr = 8e-5
 model_shortname = model_name.split("/")[-1]
 # run_name = f"{model_shortname}-DPR-{lr}"
 run_name = f"{datetime.now().strftime('%Y-%m-%d_%H-%M')}_{model_shortname}"
 output_dir = f"outputs/{model_shortname}/{run_name}/"
-model = SentenceTransformer(model_name, device="cuda")
+model = SentenceTransformer(model_name, device="mps")
 
-task_names = ["SciFact", "NFCorpus", "ArguAna", "SCIDOCS"]
+# task_names = ["SciFact", "NFCorpus", "ArguAna", "SCIDOCS"]
+task_names = ["SciFact"]
 tasks = mteb.get_tasks(tasks=task_names)
 evaluation = mteb.MTEB(tasks=tasks)
 results = evaluation.run(
